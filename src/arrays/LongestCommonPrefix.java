af3fa0758bc3977e6f14b5679dbec24b3b14c5c1
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-23 22:38:06 +0100

    conversion

--- src/arrays/LongestCommonPrefix.java
+++ src/arrays/LongestCommonPrefix.java
@@ -1,2 +1,28 @@
-package arrays;public class LongestCommonPrefix {
+package arrays;
+
+public class LongestCommonPrefix {
+
+    public static String performLongestPrefix(String[] letters) {
+
+        if (letters.length == 0){
+            return "";
+        }
+
+        int shortest = Integer.MAX_VALUE;
+        for (String str : letters){
+            shortest = Math.min(shortest, str.length());
+        }
+
+        int count = 0;
+
+        while (count < shortest){
+            for (String str : letters){
+                if (str.charAt(count) != letters[0].charAt(count)){
+                    return letters[0].substring(0,count);
+                }
+            }
+            count++;
+        }
+        return letters[0].substring(0, shortest);
+    }
 }
