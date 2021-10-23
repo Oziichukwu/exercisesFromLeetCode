12f7952e38f0d5e17519d4e9c1ba972b464807b1
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-23 22:36:31 +0100

    palindrone

--- test/arrays/LongestCommonPrefixTest.java
+++ test/arrays/LongestCommonPrefixTest.java
@@ -1,2 +1,16 @@
-package arrays;public class LongestCommonPrefixTest {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class LongestCommonPrefixTest {
+
+    @Test
+    void findLongestCommonPrefix(){
+
+        String [] letters = {"abcdefgh", "aefghifk", "abcefgh"};
+
+        String expected = "a";
+        String actual = LongestCommonPrefix.performLongestPrefix(letters);
+        assertEquals(expected, actual);
+    }
 }
--- test/arrays/PalindroneTest.java
+++ test/arrays/PalindroneTest.java
@@ -1,2 +1,13 @@
-package arrays;public class PalindroneTest {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class PalindroneTest {
+
+    @Test
+    void performPalindroneTest(){
+
+        int numbers = 121;
+        assertTrue(Palindrone.performPalindrone(numbers));
+    }
 }
