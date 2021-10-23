5bf598e75d5375122084852349b776aba14ff7d4
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-23 11:42:46 +0100

    sorted array

--- src/arrays/CalculateSumOfIntegers.java
+++ src/arrays/CalculateSumOfIntegers.java
@@ -1,2 +1,25 @@
-package arrays;public class CalculateSumOfIntegers {
+package arrays;
+
+public class CalculateSumOfIntegers {
+
+    public static int performSumOfAllIntegersInaString(String str) {
+
+
+        int sum = 0;
+        char [] charArray = str.toCharArray();
+        String temp = "";
+
+        for (int i = 0; i < charArray.length; i++) {
+            char myChar = charArray[i];
+            if (Character.isDigit(myChar)){
+
+                temp += myChar;
+            }else {
+                if (temp != ""){
+                    sum+= Integer.parseInt(temp);
+                }
+            }
+        }
+        return sum;
+    }
 }
--- test/arrays/MedianOfSortedArrayTest.java
+++ test/arrays/MedianOfSortedArrayTest.java
@@ -1,2 +1,15 @@
-package arrays;public class MedianOfSortedArray {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class MedianOfSortedArrayTest {
+
+    @Test
+    public void medianOfSortedArrayTest(){
+
+        int [] nums1 = {1,2};
+        int [] nums2 = {3,4};
+
+        assertEquals(2.5, MedianOfSortedArray.findMedianOfArray(nums1, nums2));
+    }
 }
