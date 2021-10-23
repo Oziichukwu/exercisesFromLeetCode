e2a04134e76ab2784ef92fcd4ff3c1668b0fb732
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-23 22:37:18 +0100

    container

--- src/arrays/Palindrone.java
+++ src/arrays/Palindrone.java
@@ -1,2 +1,19 @@
-package arrays;public class Palindrone {
+package arrays;
+
+public class Palindrone {
+
+    public static boolean performPalindrone(int numbers) {
+
+        Integer num = numbers;
+
+       String word =  num.toString();
+
+        for (int i = 0, j = word.length()-1; i < j; i++, j--) {
+
+            if (word.charAt(i) != word.charAt(j)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- test/arrays/ContainerWithMostWaterTest.java
+++ test/arrays/ContainerWithMostWaterTest.java
@@ -1,2 +1,16 @@
-package arrays;public class ContainerWithMostWaterTest {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class ContainerWithMostWaterTest {
+
+    @Test
+    void containerWithMostWaterTest(){
+
+        int [] height = {5,3,2,1,4};
+        assertEquals(16, ContainerWithMostWater.performOperation(height));
+
+
+    }
+
 }
