527952e5af490e5e810198404a123372213ec898
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-24 22:38:17 +0100

    duplicate

--- test/arrays/PhoneLettersCombinationTest.java
+++ test/arrays/PhoneLettersCombinationTest.java
@@ -1,2 +1,17 @@
-package arrays;public class PhoneLettersCombinationTest {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+public class PhoneLettersCombinationTest {
+
+    @Test
+    void phoneLettersCombinationTest(){
+
+        String digits = "25";
+        List<String> actual = PhoneLettersCombination.performPhoneLetterCombination(digits);
+        List<String> expected = Arrays.asList("aj","bj","cj","ak","bk","ck","al","bl","cl");
+        assertEquals(expected,actual);
+    }
 }
--- test/arrays/RemoveDuplicateFromSortedArrayTest.java
+++ test/arrays/RemoveDuplicateFromSortedArrayTest.java
@@ -1,2 +1,17 @@
-package arrays;public class RemoveDuplicateFromSortedArrayTest {
+package arrays;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class RemoveDuplicateFromSortedArrayTest {
+
+    @Test
+    void removeDuplicateFromSortedArrayTest(){
+
+        int[] numbers = { 1, 1, 2, 2, 2, 3, 3 };
+        int actual = RemoveDuplicateFromSortedArray.performRemovalOfDuplicateElement(numbers);
+        int expected = 3;
+        int[] exps = { 1, 2, 3 };
+        assertEquals(actual, expected);
+        assertArrayEquals(exps, Arrays.copyOfRange(numbers, 0, expected));
+    }
 }
