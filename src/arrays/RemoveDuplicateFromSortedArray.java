96c24a899d311c73cd7b37fd12c62a9bfd8ce0d6
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-24 22:39:36 +0100

    duplicate

--- src/arrays/RemoveDuplicateFromSortedArray.java
+++ src/arrays/RemoveDuplicateFromSortedArray.java
@@ -1,2 +1,17 @@
-package arrays;public class RemoveDuplicateFromSortedArray {
+package arrays;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateFromSortedArray {
+
+
+    public static int performRemovalOfDuplicateElement(int [] numbers){
+        int j = -1;
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == 0 || numbers[i] != numbers[i-1]){
+                numbers[++j] = numbers[i];
+            }
+        }
+        return j+1;
+    }
 }
