d2a4ad5de24fd814e5872b539dc608e0c2a421f1
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-10-26 17:19:13 +0100

    phone

--- src/arrays/PhoneLettersCombination.java
+++ src/arrays/PhoneLettersCombination.java
@@ -1,2 +1,31 @@
-package arrays;public class PhoneLettersCombination {
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneLettersCombination {
+
+    public static final String [] mapping = {"","","abc","def","ghi","jkl","mno","pqrs"
+    ,"tuv","wxyz"};
+
+    public static List<String> performPhoneLetterCombination(String digits) {
+
+        List<String> currentPosition = new ArrayList<>();
+        if (digits.length()==0){
+            return currentPosition;
+        }
+        currentPosition.add("");
+        for (int i = 0; i < digits.length(); i++) {
+            String str = mapping[digits.charAt(i) - '0'];
+            List<String> nextLetter = new ArrayList<>();
+            for (int j = 0; j < str.length(); j++) {
+                char letter = str.charAt(j);
+                for (String combination : currentPosition){
+                    nextLetter.add(combination + letter);
+                }
+            }
+            currentPosition = nextLetter;
+        }
+        return currentPosition;
+    }
 }
