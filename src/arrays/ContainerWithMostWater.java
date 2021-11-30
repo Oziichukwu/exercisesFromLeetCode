298b911d717d32b5db51396a6d0b2d3de23da63e
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-30 01:57:30 +0100

    added

--- src/arrays/ContainerWithMostWater.java
+++ src/arrays/ContainerWithMostWater.java
@@ -1,2 +1,28 @@
-package arrays;public class ContainerWithMostWater {
+package arrays;
+
+public class ContainerWithMostWater {
+
+
+    public static int performOperation(int[] height) {
+
+        int left = 0;
+
+        int right = height.length-1;
+
+        int result = 0;
+
+        while (left < right){
+
+          result = Math.max(Math.min(height[left],height[right]) * (right - left), result);
+
+          if (height[left] < height[right]){
+
+              left++;
+          }else {
+              right--;
+          }
+        }
+
+        return result;
+    }
 }
