81778adca172d005d54afbbaaf63586f1efe7750
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-30 01:54:47 +0100

    added

--- test/arrays/IntergersToWordsTest.java
+++ test/arrays/IntergersToWordsTest.java
@@ -1,2 +1,19 @@
-package arrays;public class IntergersToWordsTest {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IntergersToWordsTest {
+
+    @Test
+    void testThatIntegersCanBeConvertedToWords() {
+
+        int number = 126;
+
+        String actual = IntergersToWords.performConversionToWords(number);
+
+        String expected = "One Hundred Twenty Six";
+
+        assertEquals(expected, actual);
+    }
 }
--- test/arrays/RemoveElementTest.java
+++ test/arrays/RemoveElementTest.java
@@ -1,2 +1,16 @@
-package arrays;public class RemoveElementTest {
+package arrays;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class RemoveElementTest {
+
+    @Test
+    void removeElement(){
+
+        int [] numbers = {1,1,1,2,2,2};
+        int numberOfValues = 2;
+        int actual = RemoveElement.removeElement(numbers,numberOfValues);
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
 }
