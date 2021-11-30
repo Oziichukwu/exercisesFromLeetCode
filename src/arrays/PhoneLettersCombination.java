5a19b03778d1e4f7d9b12a7cbc3e4b0a3bf7416a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-30 01:55:21 +0100

    added

--- src/arrays/PhoneLettersCombination.java
+++ src/arrays/PhoneLettersCombination.java
@@ -5,27 +5,29 @@ import java.util.List;
 
 public class PhoneLettersCombination {
 
-    public static final String [] mapping = {"","","abc","def","ghi","jkl","mno","pqrs"
-    ,"tuv","wxyz"};
+    private static final String [] map = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
 
     public static List<String> performPhoneLetterCombination(String digits) {
 
-        List<String> currentPosition = new ArrayList<>();
-        if (digits.length()==0){
-            return currentPosition;
+        List<String> list = new ArrayList<>();
+
+        if (digits == null){
+            return list;
         }
-        currentPosition.add("");
+        list.add("");
         for (int i = 0; i < digits.length(); i++) {
-            String str = mapping[digits.charAt(i) - '0'];
-            List<String> nextLetter = new ArrayList<>();
+            String str = map[digits.charAt(i) - '0'];
+
+            List<String> next = new ArrayList<>();
+
             for (int j = 0; j < str.length(); j++) {
-                char letter = str.charAt(j);
-                for (String combination : currentPosition){
-                    nextLetter.add(combination + letter);
+                char value = str.charAt(j);
+                for(String character : list){
+                    next.add(character + value);
                 }
             }
-            currentPosition = nextLetter;
+            list = next;
         }
-        return currentPosition;
+       return list;
     }
 }
--- src/arrays/RemoveElement.java
+++ src/arrays/RemoveElement.java
@@ -1,2 +1,14 @@
-package arrays;public class RemoveElement {
+package arrays;
+
+public class RemoveElement {
+    public static int removeElement(int[] numbers, int numberOfValues) {
+
+        int len = numbers.length;
+        for (int i = 0; i <len ; i++) {
+            while (numbers[i] == numberOfValues && i < len){
+                numbers[i] = numbers[--len];
+            }
+        }
+        return len;
+    }
 }
