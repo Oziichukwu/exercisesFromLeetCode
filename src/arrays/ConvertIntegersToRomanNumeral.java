103aff428028adf816efbd7b705909370569646a
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-30 01:58:44 +0100

    added

--- src/arrays/ConvertIntegersToRomanNumeral.java
+++ src/arrays/ConvertIntegersToRomanNumeral.java
@@ -5,24 +5,23 @@ public class ConvertIntegersToRomanNumeral {
 
     public static String convertToRomanNumeral(int integer) {
 
-        int [] arabicNumerals = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        int [] romanNumeral = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
-        String [] romanNumerals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String [] arabicNumeral = {"M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder stringBuilder = new StringBuilder();
 
         int count = 0;
-
         while (integer > 0){
-            for (int i = 0; i < 13; i++) {
 
-                if (integer >= arabicNumerals[i]){
-                    integer -= arabicNumerals[i];
-                    stringBuilder.append(romanNumerals[i]);
+            for (int i = 0; i < 13; i++) {
+                if (integer >= romanNumeral[i]){
+                    integer -= romanNumeral[i];
+                    stringBuilder.append(arabicNumeral[i]);
                     break;
                 }
-                count++;
             }
+            count++;
         }
         return stringBuilder.toString();
     }
